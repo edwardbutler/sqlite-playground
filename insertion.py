e406663699f125aaa2dcb56ab51fc36c9c597bec
Edward Butler <eob1@rice.edu>
2017-12-22 15:50:01 -0600

    Update program to insert people with random ages

--- insertion.py
+++ insertion.py
@@ -1,23 +1,39 @@
 import sqlite3
+import random 
 
 # Create a connection and a cursor
-conn = sqlite3.connect('staticData.db')
+conn = sqlite3.connect('users.db')
 cursor = conn.cursor()
 
 # Function to create a static table
 def create_table():
-	cursor.execute("CREATE TABLE IF NOT EXISTS people(ID INTEGER PRIMARY KEY, Name TEXT);")
+	cursor.execute("CREATE TABLE IF NOT EXISTS people(ID INTEGER PRIMARY KEY, Name TEXT, Age REAL);")
 
 # Function to make a static insertion
-def insert_person():
-	cursor.execute("INSERT INTO people Values(1,'Edward Butler')")
+def insert_me():
+	cursor.execute("INSERT INTO people Values(1,'Edward Butler', 19)")
 	conn.commit()
 	cursor.close()
 	conn.close()
 
+def insert_random_me(id):
+	random_age = random.randrange(100)
+	cursor.execute("INSERT INTO people (ID, Name, Age) Values (?,?,?)", (id, "Edward Butler", random_age))
+
+
 # Create table and make entry
 create_table()
-insert_person()
+
+# Insert 10 Edward's with random ages
+for i in range(10):
+	insert_random_me(i)
+
+# Commit changes
+conn.commit()
+
+# Close cursor and connection
+cursor.close()
+conn.close()
 
 
 
